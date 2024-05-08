73cc89049cc747f746a000df6b5c2bed2ffa3d5e
Mateo Congo <aidenperse@gmail.com>
2024-05-08 16:26:30 -0500

    Actualización módulo users

--- Base/users.py
+++ Base/users.py
@@ -1,36 +1,36 @@
-# Importar FastAPI y Pydantic's BaseModel
+# Importaciones
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 # Crear una instancia de la aplicación FastAPI
 app = FastAPI()
 
-# Nota: Para ejecutar el servidor, use el comando: python -m uvicorn users:app --reload
+# Nota: Para ejecutar el servidor, use the command: python -m uvicorn users:app --reload
 
 # Definir una entidad de usuario utilizando Pydantic's BaseModel
 class User(BaseModel):
     """
     Representa un usuario con los siguientes atributos:
     - id: int
-    - nombre: str
-    - apellido: str
-    - edad: int
-    - correo electrónico: str
+    - first_name: str
+    - last_name: str
+    - age: int
+    - email: str
     """
     id: int
-    nombre: str
-    apellido: str
-    edad: int
-    correo_electronico: str
+    first_name: str
+    last_name: str
+    age: int
+    email: str
 
 # Inicializar una lista de usuarios
-user_list = [
+users_list = [
     # Usuario 1
-    User(id=1, nombre="Joaquin", apellido="Palomo", edad=23, correo_electronico="joaqui.palomo@gmail.com"),
+    User(id=1, first_name="Joaquin", last_name="Palomo", age=23, email="joaqui.palomo@gmail.com"),
     # Usuario 2
-    User(id=2, nombre="Nestor", apellido="Galarza", edad=23, correo_electronico="nestor.galarza@gmail.com"),
+    User(id=2, first_name="Nestor", last_name="Galarza", age=23, email="nestor.galarza@gmail.com"),
     # Usuario 3
-    User(id=3, nombre="Pedro", apellido="Gualli", edad=23, correo_electronico="pedro.gualli@gmail.com")
+    User(id=3, first_name="Pedro", last_name="Gualli", age=23, email="pedro.gualli@gmail.com")
 ]
 
 # Definir un endpoint para devolver una lista de usuarios en formato JSON
@@ -40,9 +40,9 @@ async def users_json():
     Devuelve una lista de usuarios en formato JSON.
     """
     return [
-        {"nombre": "Juan", "apellido": "Casallas", "edad": 32, "correo_electronico": "juan.casallas@gmail.com"},
-        {"nombre": "Hernesto", "apellido": "Ramirez", "edad": 20, "correo_electronico": "hernesto.ramirez@gmail.com"},
-        {"nombre": "Ricardo", "apellido": "Perez", "edad": 21, "correo_electronico": "ricardo.perez@gmail.com"}
+        {"first_name": "Juan", "last_name": "Casallas", "age": 32, "email": "juan.casallas@gmail.com"},
+        {"first_name": "Hernesto", "last_name": "Ramirez", "age": 20, "email": "hernesto.ramirez@gmail.com"},
+        {"first_name": "Ricardo", "last_name": "Perez", "age": 21, "email": "ricardo.perez@gmail.com"}
     ]
 
 # Definir un endpoint para devolver la lista de usuarios
@@ -51,7 +51,7 @@ async def users():
     """
     Devuelve la lista de usuarios.
     """
-    return user_list
+    return users_list
 
 # Definir un endpoint para recuperar un usuario por ID utilizando parámetros de ruta
 @app.get("/user/{id}")
@@ -61,7 +61,7 @@ async def users(id: int):
     :param id: int
     :return: Objeto de usuario o mensaje de error
     """
-    users = filter(lambda user: user.id == id, user_list)
+    users = filter(lambda user: user.id == id, users_list)
     try:
         return list(users)[0]
     except:
@@ -77,8 +77,6 @@ async def users(id: int):
     """
     return search_user(id)
 
-
-
 # Definir una función auxiliar para buscar un usuario por ID
 def search_user(id: int):
     """
@@ -86,27 +84,29 @@ def search_user(id: int):
     :param id: int
     :return: Objeto de usuario o mensaje de error
     """
-    users = filter(lambda user: user.id == id, user_list)
+    users = filter(lambda user: user.id == id, users_list)
     try:
         return list(users)[0]
     except:
         return {"Error: No se ha encontrado el usuario"}
-    
+
+# Definir un endpoint para crear un usuario
 @app.post("/user/")
 async def create_user(user: User):
     if type(search_user(user.id)) == User:
         return{"Error": "El usuario ya existe"}
     else:
-        user_list.append(user)
+        users_list.append(user)
 
+# Definir un endpoint para actualizar un usuario
 @app.put("/user/")
 async def update_user(user: User):
 
     found = False
 
-    for index, saved_user in enumerate(user_list):
+    for index, saved_user in enumerate(users_list):
         if saved_user.id == user.id:
-            user_list[index] = user
+            users_list[index] = user
             found = True
 
     if not found:
@@ -114,15 +114,16 @@ async def update_user(user: User):
     else:
         return user
 
+# Definir un endpoint para eliminar un usuario
 @app.delete("/user/{id}")
 async def delete_user(id: int):
 
     found = False
 
-    for index, saved_user in enumerate(user_list):
+    for index, saved_user in enumerate(users_list):
         if saved_user.id == id:
-            del user_list[index]
+            del users_list[index]
             found = True
-        
+
     if not found:
-        return {"Error": "No se ha eliminado el usuario"}    
+        return {"Error": "No se ha eliminado el usuario"}
